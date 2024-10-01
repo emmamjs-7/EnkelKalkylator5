e24a797fbe08df47d7f85a717bd29f62a06978cf
emmamjs-7 <emmaamjs@gmail.com>
2024-10-01 10:24:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace EnkelKalkylator
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enkel Kalkylator");
+            Console.WriteLine("Välj operation: (+, -, *, /");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nAnge första talet:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ange andra talet:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Ogiltlig operation");
+                    return;
+
+
+            }
+
+            Console.WriteLine($"Resultatet: {result}");
+            Console.ReadKey();
+
+        }
+    }
+}
